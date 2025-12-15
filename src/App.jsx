530931d665382602d3faa380238744d5fe642dff
riccardo-ottavi <ottavi.ps@libero.it>
2025-12-15 14:12:15 +0100

    completa ex2

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,28 @@ function App() {
     return dataFetch
   }
 
+  function lanciaDado(){
+    const lancio = new Promise((resolve, reject) => {
+    console.log("lanncio il dado...")
+    
+      setTimeout(() => {
+        const probability = Math.random()
+        console.log(probability)
+        const risultato = Math.floor(Math.random() * 6) + 1
+        if(probability <= 0.2) {
+          reject("il dado si è inceppato!")
+        }else{
+          resolve(risultato)
+        }
+      }, 3000)
+    })
+    return lancio
+  }
+
+  lanciaDado()
+  .then(diceRes => console.log("il risultato è:", diceRes))
+  .catch(error => console.log(error))
+
 
   getPostTitle(1)
   .then(obj => console.log(obj))
