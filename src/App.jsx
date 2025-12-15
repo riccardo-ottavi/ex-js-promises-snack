1841620838b19ea728b5bb1f58597b9a8281176b
riccardo-ottavi <ottavi.ps@libero.it>
2025-12-15 14:33:49 +0100

    miglioramenti vari

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
-
   function getPostTitle(id) {
     const dataFetch = new Promise((resolve, reject) => {
       axios.get(`https://dummyjson.com/posts/${id}`)
         .then(response => response.data)
-        .then(obj => resolve(obj.title))
+        .then(post => resolve(post.title))
         .catch(reject)
     })
     return dataFetch
@@ -16,14 +15,14 @@ function App() {
   function lanciaDado(){
     const lancio = new Promise((resolve, reject) => {
     console.log("lanncio il dado...")
-    
+  
       setTimeout(() => {
         const probability = Math.random()
         console.log(probability)
-        const risultato = Math.floor(Math.random() * 6) + 1
         if(probability <= 0.2) {
           reject("il dado si è inceppato!")
         }else{
+          const risultato = Math.floor(Math.random() * 6) + 1
           resolve(risultato)
         }
       }, 3000)
@@ -37,7 +36,7 @@ function App() {
 
 
   getPostTitle(1)
-  .then(obj => console.log(obj))
+  .then(title => console.log(title))
   .catch(error => console.log(error))
 
   return (
